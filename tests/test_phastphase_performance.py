c086713c4d729470ed7b11a76358741efdc3d6ab
Tore Barach Kamar <tore.barach.kamar@gmail.com>
2025-12-18 15:31:03 +0000

    HIO Tests WIP

--- tests/test_phastphase_performance.py
+++ tests/test_phastphase_performance.py
@@ -3,6 +3,7 @@ import os
 os.environ['XLA_PYTHON_CLIENT_PREALLOCATE'] = 'false'
 os.environ['XLA_PYTHON_CLIENT_MEM_FRACTION'] = '0.75'  # Use 75% of GPU memory
 
+import random
 import time
 import datetime
 import sqlite3
@@ -27,12 +28,13 @@ from phastphase.retrieval_jax._alternating_projections import *
 SELECTED_TEST_CASES = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 SELECTED_DESCENT_METHODS = [0]
 SELECTED_WIND_METHODS = [0]
-SELECTED_MAX_ITERS = [100]
+SELECTED_MAX_ITERS = [1000]
 SELECTED_GRAD_TOLERANCES = [1e-8]
 SELECTED_SHOULD_GUESS_WIND = [False]
 SELECTED_FOURIER_OVERSAMPLES = [2]
 SELECTED_CONVERGENCE_TOLERANCES = [5e-4]
-SELECTED_MAX_RANDOM_RESTARTS = [100]
+SELECTED_MAX_RANDOM_RESTARTS = [1000]
+SELECTED_BETAS = [0.36, 0.4, 0.5, 1.17]
 
 RANDOM_GENERATOR = np.random.default_rng(0)
 
@@ -131,6 +133,21 @@ class ResultsDatabase:
         self.conn.close()
 
 
+class CachedGenerator:
+    def __init__(self, iterable):
+        self.iterable = iter(iterable)
+        self.cache = []
+
+    def __iter__(self):
+        # First, yield everything we've already computed
+        yield from self.cache
+        
+        # Then, continue computing new values and caching them
+        for item in self.iterable:
+            self.cache.append(item)
+            yield item
+
+            
 def pytest_generate_tests(metafunc):
     """Dynamically parametrize test cases from the most recent generated dataset.
 
@@ -175,77 +192,96 @@ def pytest_generate_tests(metafunc):
 
     metafunc.parametrize("case_id,case_file", 
         [(case_idx, cf) for case_idx, (case_file_params, _) in case_data.items() for cf in case_file_params],
-        ids=[f"case_{case_idx}-{cf}" for case_idx, (_, case_file_ids) in case_data.items() for cf in case_file_ids]
+        ids=[f"case_{case_idx}-{cf}" for case_idx, (_, case_file_ids) in case_data.items() for cf in case_file_ids],
+        scope="session"
     )
 
 
-@pytest.fixture(params=SELECTED_DESCENT_METHODS)
+@pytest.fixture(scope="session", params=SELECTED_DESCENT_METHODS)
 def descent_method(request):
     """Fixture for descent method."""
     return request.param
 
 
-@pytest.fixture(params=SELECTED_MAX_ITERS)
+@pytest.fixture(scope="session", params=SELECTED_MAX_ITERS)
 def max_iters(request):
     """Fixture for maximum iterations."""
     return request.param
 
 
-@pytest.fixture(params=SELECTED_GRAD_TOLERANCES)
+@pytest.fixture(scope="session", params=SELECTED_GRAD_TOLERANCES)
 def grad_tolerance(request):
     """Fixture for gradient tolerance."""
     return request.param
 
 
-@pytest.fixture(params=SELECTED_WIND_METHODS)
+@pytest.fixture(scope="session", params=SELECTED_WIND_METHODS)
 def wind_method(request):
     """Fixture for winding method."""
     return request.param
 
 
-@pytest.fixture(params=SELECTED_SHOULD_GUESS_WIND)
+@pytest.fixture(scope="session", params=SELECTED_SHOULD_GUESS_WIND)
 def should_guess_wind(request):    
     """Fixture for whether to guess winding number."""
     return request.param
 
 
-@pytest.fixture(params=SELECTED_FOURIER_OVERSAMPLES)
+@pytest.fixture(scope="session", params=SELECTED_FOURIER_OVERSAMPLES)
 def fourier_oversample(request):
     """Fixture for Fourier oversampling factor."""
     return request.param
 
 
-@pytest.fixture(params=SELECTED_CONVERGENCE_TOLERANCES)
+@pytest.fixture(scope="session", params=SELECTED_CONVERGENCE_TOLERANCES)
 def convergence_tolerance(request):
     """Fixture for convergence tolerance."""
     return request.param
 
 
-@pytest.fixture(params=SELECTED_MAX_RANDOM_RESTARTS)
+@pytest.fixture(scope="session", params=SELECTED_MAX_RANDOM_RESTARTS)
 def max_random_restarts(request):
     """Fixture for maximum random restarts."""
-    return request.param
+    return request.param    
 
 
-@pytest.fixture
-def case_data(case_file):
+@pytest.fixture(scope="session")
+def betas():
+    return iter(SELECTED_BETAS)
+
+
+@pytest.fixture(scope="session")
+def case_data(case_id, case_file):
     """Fixture to load dataset from case_file."""
     return np.load(case_file)
 
     
-@pytest.fixture
+@pytest.fixture(scope="session")
 def near_field(case_data):
     """Fixture to extract near_field from case_data."""
     return jnp.asarray(case_data["near_field"], dtype=jnp.complex128)
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
+def near_field_padded(near_field, fourier_oversample):
+    """Fixture to extract near_field from case_data."""
+    padded_near_field = jnp.pad(
+        near_field, 
+        ((0, near_field.shape[0] * (fourier_oversample - 1)), (0, near_field.shape[1] * (fourier_oversample - 1))), 
+        mode='constant', 
+        constant_values=0
+    )
+
+    return padded_near_field
+
+
+@pytest.fixture(scope="session")
 def near_field_md5(case_data):
     """Fixture to extract near_field from case_data."""
     return str(case_data["near_field_md5"])
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def far_field(near_field):
     """Fixture to compute far_field from near_field."""
         # build far-field intensity (oversample by specified factor)
@@ -253,7 +289,7 @@ def far_field(near_field):
     return y / (jnp.shape(y)[0]*jnp.shape(y)[1])
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def far_field_oversampled(near_field, fourier_oversample):
     """Fixture to compute far_field from near_field."""
         # build far-field intensity (oversample by specified factor)
@@ -261,7 +297,7 @@ def far_field_oversampled(near_field, fourier_oversample):
     return y / (jnp.shape(y)[0]*jnp.shape(y)[1])
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def support_mask(near_field):
     # Use a full mask.
     # TODO: Maybe calculate support from image?
@@ -269,7 +305,19 @@ def support_mask(near_field):
     return mask
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
+def padded_support_mask(support_mask, fourier_oversample):
+    padded_mask = jnp.pad(
+        support_mask, 
+        ((0, support_mask.shape[0] * (fourier_oversample - 1)), (0, support_mask.shape[1] * (fourier_oversample - 1))), 
+        mode='constant', 
+        constant_values=0
+    )
+
+    return padded_mask
+
+
+@pytest.fixture(scope="session")
 def winding_guess(case_data, should_guess_wind):
     """Fixture to extract winding guess from case_data."""
     bright_spot = case_data["spot_center"]
@@ -277,11 +325,12 @@ def winding_guess(case_data, should_guess_wind):
     return winding_guess
 
 
-@pytest.fixture(params=ACTIVE_INIT_METHODS, ids=[m[0] for m in ACTIVE_INIT_METHODS])
-def initial_guess(request, near_field, far_field_oversampled, max_random_restarts):
+@pytest.fixture(scope="session", params=ACTIVE_INIT_METHODS, ids=[m[0] for m in ACTIVE_INIT_METHODS])
+def initial_guess_data(request, near_field, far_field_oversampled, max_random_restarts):
     """
     This fixture returns the actual x0 vector.
     """
+    # FIXME: We will always be generating the same sequence of random numbers!
     key = jax.random.PRNGKey(0)
 
     method_name, method_func, _ = request.param
@@ -305,10 +354,32 @@ def initial_guess(request, near_field, far_field_oversampled, max_random_restart
             )
 
     # Return the iterator object so the test can loop over it
-    return guess_generator()
+    return CachedGenerator(guess_generator())
 
 
-@pytest.fixture(params=ACTIVE_FLOW_METHODS, ids=[m[0] for m in ACTIVE_FLOW_METHODS])
+@pytest.fixture(scope="function")
+def initial_guess(initial_guess_data):
+    """
+    This fixture returns the actual x0 vector.
+    """
+    return iter(initial_guess_data)
+
+
+@pytest.fixture(scope="function")
+def padded_initial_guess(initial_guess, fourier_oversample):
+    def padded_guess_generator():
+        for guess in initial_guess:
+            yield jnp.pad(
+                guess, 
+                ((0, guess.shape[0] * (fourier_oversample - 1)), (0, guess.shape[1] * (fourier_oversample - 1))), 
+            mode='constant', 
+            constant_values=0
+        )
+
+    return padded_guess_generator()
+
+
+@pytest.fixture(scope="session", params=ACTIVE_FLOW_METHODS, ids=[m[0] for m in ACTIVE_FLOW_METHODS])
 def flow_method(request):
     method_name, method_func, _ = request.param
     return method_func
@@ -512,7 +583,110 @@ def test_gradient_flow_retrieve(
                     "max_random_restarts": max_random_restarts,
                 },
                 output=False,
-                save_to_db=True,
+                save_to_db=False,
+            )
+            if success:
+                # Found a solution that converges.
+                min_err = err
+                min_prediction = x_out
+                min_t = t
+                break
+            else:
+                # Capture error and continue to the next guess from the fixture
+                if err < min_err:
+                    min_err = err
+                    min_prediction = x_out
+                    min_t = t
+
+    except Exception as exc:  # pragma: no cover - surface errors as test failures
+        pytest.fail(f"retrieve raised an exception: {exc}")
+        return
+
+    # If we exhaust the generator without returning, the test has failed.
+    evaluate_convergence(
+        prediction=min_prediction,
+        ground_truth=near_field,
+        timer_obj=min_t,
+        method_name=flow_method_name,
+        init_method_name=init_method_name,
+        num_attempts=attempt + 1,
+        metadata={
+            "max_random_restarts": max_random_restarts,
+        },
+        output=True,
+        save_to_db=True,
+    )
+
+
+def test_gradient_flow_sanity(
+    request,
+    case_id: int,
+    case_file: str,
+    near_field: jnp.ndarray,
+    far_field_oversampled: jnp.ndarray,
+    flow_method,
+    # initial_guess: iter,
+    grad_tolerance: float,
+    max_iters: int, 
+    max_random_restarts: int,
+    execution_timer,
+    evaluate_convergence
+):
+    """Helper: run retrieve() on one saved near-field object and perform checks.
+
+    Returns the (err, duration) tuple for reporting if needed.
+    """
+    init_method_name = "sanity" #request.node.callspec.params["initial_guess"][0]
+    flow_method_name = request.node.callspec.params["flow_method"][0]
+    
+    min_err = float('inf')
+    min_prediction = None
+    min_t = None
+
+    x_start = random.randint(0, int(near_field.shape[0] / 2))
+    y_start = random.randint(0, int(near_field.shape[1] / 2))
+    k = 300
+    # 1. Initialize array with a complex dtype
+    key = jax.random.PRNGKey(0)
+
+    # 2. Split key and generate complex random values
+    key, subkey = jax.random.split(key)
+    # jax.random.normal supports complex dtypes directly
+    # It generates z = x + iy where x, y ~ N(0, 1/sqrt(2))
+    random_complex = jax.random.normal(subkey, (k, k), dtype=jnp.complex64)
+
+    # Wrap to [-pi, pi]
+    imag_part = np.angle(random_complex)
+
+    # Combine into a complex-valued array
+    complex_map = np.abs(random_complex) * np.exp(1j * imag_part)
+    
+    near_field_mod = near_field.at[x_start: x_start + k, y_start: y_start + k].set(complex_map)
+    initial_guess = [near_field_mod]  # Sanity check with perfect data
+    # far_field_oversampled = near_field  # Sanity check with perfect data
+
+    try:
+        for attempt, x0 in enumerate(initial_guess):          
+            with execution_timer as t:
+                x_out = flow_method(
+                    x0,
+                    far_field_oversampled,
+                    grad_tolerance=grad_tolerance,
+                    iter_limit=max_iters,
+                )
+
+            err, success = evaluate_convergence(
+                prediction=x_out,
+                ground_truth=near_field,
+                timer_obj=t,
+                method_name=flow_method_name,
+                init_method_name=init_method_name,
+                num_attempts=attempt + 1,
+                metadata={
+                    "max_random_restarts": max_random_restarts,
+                },
+                output=False,
+                save_to_db=False,
             )
             if success:
                 # Found a solution that converges.
@@ -552,9 +726,11 @@ def test_HIO_retrieve(
     case_id: int,
     case_file: str,
     near_field: jnp.ndarray,
-    far_field: jnp.ndarray,
-    flow_method,
-    initial_guess: iter,
+    near_field_padded: jnp.ndarray,
+    far_field_oversampled: jnp.ndarray,
+    padded_support_mask: jnp.ndarray,
+    padded_initial_guess: iter,
+    betas: iter,
     grad_tolerance: float,
     max_iters: int, 
     max_random_restarts: int,
@@ -565,40 +741,52 @@ def test_HIO_retrieve(
 
     Returns the (err, duration) tuple for reporting if needed.
     """
-    init_method_name = request.node.callspec.params["initial_guess"][0]
-    flow_method_name = request.node.callspec.params["flow_method"][0]
+    init_method_name = request.node.callspec.params["initial_guess_data"][0]
     
     min_err = float('inf')
     min_prediction = None
     min_t = None
 
     try:
-        for attempt, x0 in enumerate(initial_guess):          
+        for attempt, (x0, beta) in enumerate(zip(padded_initial_guess, betas)):          
             with execution_timer as t:
-                x_out = flow_method(
-                    x0,
-                    far_field,
-                    grad_tolerance=grad_tolerance,
-                    iter_limit=max_iters,
+                # import pdb; pdb.set_trace()
+                # x0 = near_field_padded
+                x_out, num_iterations, residual = HIO(
+                    x0=x0,
+                    mask=padded_support_mask,
+                    y=far_field_oversampled,
+                    beta=beta,
+                    tolerance=grad_tolerance,
+                    max_iters=max_iters,
                 )
+                import pdb; pdb.set_trace()
+                x_out_sliced = x_out[:near_field.shape[0], :near_field.shape[1]]
 
             err, success = evaluate_convergence(
-                prediction=x_out,
+                prediction=x_out_sliced,
                 ground_truth=near_field,
                 timer_obj=t,
+                method_name="HIO",
+                init_method_name=init_method_name,
+                num_attempts=attempt + 1,
+                metadata={
+                    "max_random_restarts": max_random_restarts,
+                },
                 output=False,
+                save_to_db=False,
             )
             if success:
                 # Found a solution that converges.
                 min_err = err
-                min_prediction = x_out
+                min_prediction = x_out_sliced
                 min_t = t
                 break
             else:
                 # Capture error and continue to the next guess from the fixture
                 if err < min_err:
                     min_err = err
-                    min_prediction = x_out
+                    min_prediction = x_out_sliced
                     min_t = t
 
     except Exception as exc:  # pragma: no cover - surface errors as test failures
@@ -609,13 +797,12 @@ def test_HIO_retrieve(
         prediction=min_prediction,
         ground_truth=near_field,
         timer_obj=min_t,
+        method_name="HIO",
+        init_method_name=init_method_name,
+        num_attempts=attempt + 1,
         metadata={
-            "method_name": flow_method_name,
-            "init_method_name": init_method_name,
-            "attempt": f"{attempt + 1}",
-            "max_iters": max_iters,
             "max_random_restarts": max_random_restarts,
-            "grad_tolerance": grad_tolerance,
         },
         output=True,
+        save_to_db=False,
     )
